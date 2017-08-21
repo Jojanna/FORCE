eb5db2de9365c610d7699965852bdd8e08ac03f5
Jojanna <j.wallis@dunelm.org.uk>
2017-08-21 16:09:50 +0100

    branching test

--- FRM/FRM_function.py
+++ FRM/FRM_function.py
@@ -23,6 +23,8 @@
 #   OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 #   ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+' testing the branching...'
+
 
 import numpy as np
 import pandas as pd
